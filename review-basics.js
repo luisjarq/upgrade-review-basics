1dafb693d63c77bd45995470829ebe8a0f148e6a
Luisjarq <lc.luisja@gmail.com>
2021-10-20 01:42:51 +0200

    Iteración#2

--- review-basics.js
+++ review-basics.js
@@ -17,8 +17,55 @@ let categories = [];
 for (const movie of movies) {
   for (const cat of movie.categories) {
     if (!categories.includes(cat)) {
-        categories.push(cat);
+      categories.push(cat);
     }
   }
 }
 console.log(categories);
+//Iteración#2
+const users = [
+  {
+    name: "Manolo el del bombo",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 50 },
+      rain: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Mortadelo",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 30 },
+      shower: { format: "ogg", volume: 55 },
+      train: { format: "mp3", volume: 60 },
+    },
+  },
+  {
+    name: "Super Lopez",
+    favoritesSounds: {
+      shower: { format: "mp3", volume: 50 },
+      train: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "El culebra",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 67 },
+      wind: { format: "ogg", volume: 35 },
+      firecamp: { format: "mp3", volume: 60 },
+    },
+  },
+];
+let averages = [];
+for (const user of users) {
+  let avg = 0;
+  let count = 0;
+  for (const prop in user.favoritesSounds) {
+    avg +=user.favoritesSounds[prop].volume;
+    count++;
+  }
+  averages.push(user.name, avg / count);
+}
+console.log(averages);
+
